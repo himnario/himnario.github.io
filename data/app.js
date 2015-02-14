f62461c4609f327b4773f71f0aea67c6c098edda
Chino <montano.israel@icloud.com>
2015-02-13 21:53:44 -0500

    Initial files

---
+++ data/app.js
@@ -0,0 +1,54 @@
+/*
+ * @author: Israel Montano
+ * 
+ */
+
+var song = []; // stores an id for every song.
+var songId = "";
+
+// this functions loads all the songs available
+function showData(){
+	var id, list = '<ol>';
+	for(var i = 0; i < indice.length; i++){ // get data from array
+		id = 'song' + (i + 1);
+		song.push(id);
+		list += '<li id=' + id + ' onclick="getSong(id)"><a href="#" >' + indice[i].title + '</li>';	
+	}
+	list += '</a></ol>';
+	document.querySelector('.index').innerHTML = list;
+}
+
+function getSong(id){
+	songId = id;
+	window.open("../pages/song.html");
+}
+
+// displays song 
+function displaySong(num){
+	var lyrics, songTitle = '<h1>' + indice[num - 1].title + '</h1>';
+	lyrics = '<p>' + indice[num - 1].text + '</p>';
+	document.querySelector('.title').innerHTML = songTitle;
+	document.querySelector('.song').innerHTML = lyrics;
+}
+
+var count = 0; // keeps count of the clicks
+
+function incrementCount(){
+	count++; // if the next button is click the count variable increments by 1
+}
+
+function decrementCount(){
+	count--; // if the back button is clicked the count variable decrements by 1
+	if (count < 0) count = 1 ;
+}
+
+document.querySelector("#next").addEventListener('click',function(){
+	incrementCount();
+	displaySong(count);
+});
+
+document.querySelector("#back").addEventListener('click',function(){
+	decrementCount();
+	displaySong(count);
+});
+
---
+++ data/songs.js
@@ -0,0 +1,149 @@
+
+// Aqui se guardaran los himnos
+var indice = [
+	{
+		title: "La Senda Del Justo", 
+		link: "../pages/song1.html",
+		text: "La senda del justo es cual luz de aurora <br> que va en aumento hasta que apunta el cenit <br> Hasta que el día esclarezca e ilumine nuestro ser <br> y el lucero de la mañana nazca en nuestro corazón <br><br> <span class='color'>Coro</span> <br><br> Bendita senda, senda que el Señor trazó <br> Llena de gozo paz y felicidad. <br><br>Esta bendita senda jamás la vio ave <br> ni ojo de buitre jamás la miro <br> de raíz trastorno los montes y  los ríos en su nacer <br> sus ojos vieron lo preciado y lo escondido sacó a la luz <br><br>Jesús el cordero descubrió esta senda <br> cuando en el calvario su espíritu dio <br> el velo del templo, rasgose en Jerusalén <br> Entraron todo los gentiles cual sacerdote a ministrar. <br> "
+	}, 
+
+	{
+		title: "El Fundamento Eterno",
+		link: "../pages/song2.html",
+		text: "Jesús el fundamento eterno  <br> Que viene con su grande poder <br> Servirte esos son mis deseos <br> Ayúdame mi Cristo para poder vencer <br><br> Mi alma sedienta esta de verte <br> Mi alma espera en Jehová <br> Mi alma su poder la sustenta <br> Ayúdame mi Cristo hasta la eternidad <br><br> Jesús que linda es tu palabra <br> Dulce mas dulce que la miel <br> Nos llena el corazón de vida <br> Ayúdanos mi Cristo es para serte fiel <br><br> Ahora me quedo meditando <br> Lo mucho que ama Jesús <br> Que por todo el mundo dio su vida <br> Estando en tinieblas nos traslado a su luz "
+	}, 
+
+	{
+		title: "Oh Que Gozo",
+		link: "../pages/song3.html",
+		text: "Oh que gozo el que siento en mi alma<br> Oh que gozo es un gozo incomparable<br> Oh que gozo e que siento en mi alma<br> Porque Cristo me ha salvado de la muerte<br><br> Ahora tomo con amor las armaduras<br> Las armaduras que mi Cristo me ha dejado<br> Que es temor, amor y caridad<br> Santidad, y templanza, y tolerancia<br><br> El camino es un camino muy sufrido<br> Y muy pocos los que quieren caminar<br> Pero si te conviertes con amor<br> Ningún viento los podrá arrebatar.<br><br> Todo árbol que mi padre lo ha plantado<br> Dará frutos apacibles de justicia<br> Pues a mi juicios son verdad y rectitud<br> No cualquiera los tendrá que resistir.<br><br> Pues ya poco va quedando para el fin<br> Y yo quiero esta iglesia ordenada<br> Que por luz yo la he puesto en las tinieblas<br> Como antorcha entre mucha oscuridad<br><br> Muchos pueblos se profesan ser piadosos <br> Y desechan la enseñanza de mi padre<br> Yo no vine a enseñarles en holganza <br> Sino vine a darles enseñanza con templanza<br><br> Mis caminos son muy rectos con justicia <br> Y honestos para ver la indiferencia <br> Este pueblo es conocido entre los pueblos <br> Y no hay pueblo que los pueda comparar<br><br> Y el primero de todos los ministros <br> Es Jesús el rey de Israel<br> Y los que siguen por cabezas de su pueblo<br> Que no dejen de templar siempre el orden.<br><br> Apacienta a mi pueblo con amor<br> Exhortando con espíritu de Dios <br> Que yo soy el que examino los corazones <br> Y el fruto que me da de cada uno. "
+	}, 
+
+	{
+		title: "La Esposa del Cordero",
+		link: "../pages/song4.html",
+		text: "Son pocos los días que nos quedan<br> Miremos cada uno por nuestra salvación<br> Busquemos a Dios en sacrificio <br> Y no estemos pensando que tarda el salvador.<br><br> Si duermen algunos que despierten <br> Y si andan en tinieblas que vengan a la luz<br> Si andan muy altos que se bajen<br> Para ver cara a cara a mi Señor Jesús<br><br> La esposa del cordero se prepara <br> Con su vestido blanco para ir a recibir<br> A Cristo el esposo ya no tarda<br> Lo dice su palabra que pronto ha de venir<br><br> Busquemos al que nos amó primero<br> Sirvámosle con gozo al que nos redimió<br> Sepamos que el que nos amó primero <br> En la cruz del madero su sangre derramó<br><br> Y todo el que esté limpio que se limpie<br> Y cada día que pase se limpie más y más.<br> Y todo el que esté sucio que se ensucie<br> Ha dicho el rey de gloria que a las bodas no entrará "
+	}, 
+
+	{
+		title: "Quiero entonar"
+	}, 
+
+	{
+		title: "Contame Hermano"
+	}, 
+
+	{
+		title: "Desierto"
+	}, 
+
+	{
+		title: "Amado Pueblo de Dios"
+	}, 
+	{
+		title: "El Pueblo Del Señor"
+	}, 
+
+	{
+		title: "Despertad"
+	}, 
+	
+	{
+		title: "Dulcisimo Jesus"
+	}, 
+	
+	{
+		title: "Dios Bendiga la Columna"
+	}, 
+	
+	{
+		title: "Tenemos el Fundamento"
+	}, 
+	
+	{
+		title: "A Este Pueblo"
+	}, 
+	
+	{
+		title: "Hermano Quiero"
+	}, 
+	
+	{
+		title: "A Jehova yo Cantare"
+	}, 
+	
+	{
+		title: "Que Dulce es tu Nombre"
+	}, 
+	
+	{
+		title: "Hecha tu Pan"
+	}, 
+	
+	{
+		title: "Santo es mi Dios"
+	}, 
+
+	{
+		title: "Todo el Tiempo"
+	}, 
+
+	{
+		title: "Jesucristo"
+	}, 
+
+	{
+		title: "Revelada"
+	}, 
+	{
+		title: "Soy Soldado"
+	}, 
+	
+	{
+		title: "El Dia Dura"
+	}, 
+	
+	{
+		title: "Tu Fiel Jornada"
+	}, 
+	
+	{
+		title: "Cuan Bueno"
+	}, 
+	
+	{
+		title: "Que Dulce es tu Nombre"
+	}, 
+	
+	{
+		title: "Alegrate oh Judea"
+	}, 
+	
+	{
+		title: "Por un Amor"
+	}, 
+	
+	{
+		title: "Siervo Mio"
+	}, 
+	
+	{
+		title: "Oid mis Palabras"
+	}, 
+
+	{
+		title: "Son Hermoso los Sarmientos"
+	}, 
+
+	{
+		title: "Estrella Hermosa"
+	}, 
+
+	{
+		title: "Con Este Cantico"
+	}, 
+
+	{
+		title: "Desilusiones"
+	} 
+];
